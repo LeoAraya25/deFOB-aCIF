9c7e3444f5f7e0805fa87936e9d2e0f43a5bcea7
laraya25 <133065279+laraya25@users.noreply.github.com>
2023-07-04 20:28:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+function calcularCIFCondicional(precioProducto) {
+    if (precioProducto > 0) {
+      let cif = precioProducto * 0.12;
+      return cif;
+    } else {
+      return "El precio del producto debe ser mayor que 0.";
+    }
+  }
+  
+  let precio = 500; 
+  let cifCalculado = calcularCIFCondicional(precio);
+  console.log("Precio FOB del producto: $" + precio);
+  console.log("CIF del 12%: $" + cifCalculado);
+
+function calcularCIFCiclo(precioFobProducto) {
+    if (precioFobProducto > 0) {
+      let cif = precioFobProducto * 0.12;
+      return cif;
+    } else {
+      return "El valor FOB del producto debe ser mayor que 0.";
+    }
+  }
+
+  let precioFobIngresado = prompt("Ingrese el valor FOB del producto:");
+  let precioFobProducto = parseFloat(precioFobIngresado);
+
+  if (!isNaN(precioFobProducto)) {
+    let cifCalculado = calcularCIFCiclo(precioFobProducto);
+    console.log("Precio Fob del producto: $" + precioFobProducto);
+    console.log("CIF del 12%: $" + cifCalculado);
+  } else {
+    console.log("El valor ingresado no es válido.");
+  }
+  
